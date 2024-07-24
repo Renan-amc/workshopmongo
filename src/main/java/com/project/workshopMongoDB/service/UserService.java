09008b01acfe86dbc8f1ccfc0228cade074bbedf
renan-amc <renanmv10@hotmail.com>
2024-07-24 20:30:12 -0300

    Insercao de usuario com POST

--- src/main/java/com/project/workshopMongoDB/service/UserService.java
+++ src/main/java/com/project/workshopMongoDB/service/UserService.java
@@ -41,4 +41,13 @@ public class UserService {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getFirstName(), objDto.getLastName(), null, null);
+	}
 }
