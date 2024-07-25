a5a3bd4ef71ec8bdfcb6b75c266c02a088118304
renan-amc <renanmv10@hotmail.com>
2024-07-25 20:48:46 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/project/workshopMongoDB/config/Instantiation.java
+++ src/main/java/com/project/workshopMongoDB/config/Instantiation.java
@@ -13,6 +13,7 @@ import com.project.workshopMongoDB.data.PostRepository;
 import com.project.workshopMongoDB.data.User;
 import com.project.workshopMongoDB.data.UserRepository;
 import com.project.workshopMongoDB.dto.AuthorDTO;
+import com.project.workshopMongoDB.dto.CommentDTO;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.save(post1);	
 		postRepository.save(post2);	
